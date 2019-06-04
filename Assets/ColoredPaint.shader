ef211c12c65133363e9e01060d0dc1aedf89bd85
Luke Turner <luke@lukedturner.com>
2019-06-04 20:51:51 +1000

    Initial commit, made a sine wave animation

---
+++ Assets/ColoredPaint.shader
@@ -0,0 +1,48 @@
+﻿Shader "Custom/ColoredPaint" {
+	Properties {
+		_Glossiness ("Smoothness", Range(0,1)) = 0.5
+		_Metallic ("Metallic", Range(0,1)) = 0.0
+	}
+	SubShader {
+		Tags { "RenderType"="Opaque" }
+		LOD 200
+
+		CGPROGRAM
+		// Physically based Standard lighting model, and enable shadows on all light types
+		#pragma surface surf Standard fullforwardshadows
+
+		// Use shader model 3.0 target, to get nicer looking lighting
+		#pragma target 3.0
+
+		//The input struct takes values that determine the custom data needed to colour pixels.
+		struct Input {
+			//So here, the worldposition of the cubes affects the colour scheme.
+		  float3 worldPos;
+		};
+
+		half _Glossiness;
+		half _Metallic;
+
+
+		// Add instancing support for this shader. You need to check 'Enable Instancing' on materials that use the shader.
+		// See https://docs.unity3d.com/Manual/GPUInstancing.html for more information about instancing.
+		// #pragma instancing_options assumeuniformscaling
+		UNITY_INSTANCING_BUFFER_START(Props)
+			// put more per-instance properties here
+		UNITY_INSTANCING_BUFFER_END(Props)
+
+		void surf (Input IN, inout SurfaceOutputStandard o) {
+			// Albedo comes from a texture tinted by color
+
+			//Modify the red component of the albedo (whiteness) attribute.
+			//G and B values are set to 0 already before this function is invoked.
+			o.Albedo.br = IN.worldPos.xy * 0.5 + 0.5;
+			// Metallic and smoothness come from slider variables
+			o.Metallic = _Metallic;
+			o.Smoothness = _Glossiness;
+
+		}
+		ENDCG
+	}
+	FallBack "Diffuse"
+}
---
+++ Assets/Graph.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour {
+    public Transform pointPrefab;
+    //initialize an array of transforms
+    Transform[] points;
+  
+    [Range(10,100)] public int resolution = 10;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i=0; i < points.Length; i++)
+        {
+            //Get the current point and its position
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+
+            //Derive the y position from the above values
+            //position.y = (position.x * position.x * position.x);
+
+            position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
+            //Apply the newly changed variable value to our point
+            point.localPosition = position;
+        }
+	}
+
+    void Awake()
+    {
+        //create the object associated with points, using the new keyword. the size is resolution
+        points = new Transform[resolution];
+        float step = 2f / resolution;
+        Vector3 scale = Vector3.one * step;
+        Vector3 position;
+
+        position.z = 0;
+        //We are now updating the y value in Update, so we must set it to 0 upon wake.
+        position.y = 0;
+
+        
+            for (int i=0; i < points.Length; i++)
+            {
+            Transform point = Instantiate(pointPrefab);
+
+            position.x = (i + 0.5f) * step - 1f;
+            //y is equal to x, this represents f(x) = x.
+            //position.y = position.x;
+
+            //f(x) = x^2:
+            //position.y = (position.x * position.x);
+
+            //f(x) = x^3
+           // position.y = (position.x * position.x * position.x);
+            point.localPosition = position; 
+            // use the scale variable to reduce CPU as the size stays the same
+            point.localScale = scale;
+            point.SetParent(transform,false);
+            points[i] = point;
+
+            }
+
+        
+    }
+}
